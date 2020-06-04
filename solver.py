501d48c52534bf748c9e43a6a61b0b1704e8c60b
nw <nico.walters@techtalents.es>
2020-06-04 18:08:23 +0200

    Path fixing

--- solver.py
+++ solver.py
@@ -9,6 +9,14 @@ def read_puzzle_file():
 def convert_puzzle(string):
     puzzle = []
     for i in range(9):
-        row = string[i*9 : i*10]
+        row = string[i*9 : i*9+9]
         puzzle.append(row)
     print(puzzle)
+
+
+import os
+correct_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(correct_dir)
+
+puzzles = read_puzzle_file()
+convert_puzzle(puzzles[10])
