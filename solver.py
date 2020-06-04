9a9a059517d80989999e9dce78e15043f7730bce
nw <nico.walters@techtalents.es>
2020-06-04 18:49:01 +0200

    Progrss

--- solver.py
+++ solver.py
@@ -1,6 +1,8 @@
 # heuristics
 # backtracking
 
+from pprint import pprint
+
 def read_puzzle_file():
     with open("puzzles") as file:
         content = file.read().splitlines()
@@ -13,7 +15,16 @@ def convert_puzzle(string):
         row = list(row)
         row = list(map(int, row))
         puzzle.append(row)
-    print(puzzle)
+    return puzzle
+
+
+def check_pos(puzzle, x, y, n):
+    row = puzzle[y]
+    column = [row[x] for row in puzzle]
+    i = x // 3
+    j = y // 3
+    print(puzzle[j*3 : j*3+3])
+
 
 
 if __name__ == "__main__":
@@ -22,4 +33,8 @@ if __name__ == "__main__":
     os.chdir(correct_dir)
 
     puzzles = read_puzzle_file()
-    convert_puzzle(puzzles[10])
+
+    puzzle = convert_puzzle(puzzles[10])
+
+    pprint(puzzle)
+    check_pos(puzzle, 2, 6, None)
