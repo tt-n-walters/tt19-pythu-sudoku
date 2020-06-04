c166085df07016f887235cbee4c03aad103d5c02
nw <nico.walters@techtalents.es>
2020-06-04 17:56:54 +0200

    Converting puzzle string

--- solver.py
+++ solver.py
@@ -5,3 +5,10 @@ def read_puzzle_file():
     with open("puzzles") as file:
         content = file.read().splitlines()
         return content
+
+def convert_puzzle(string):
+    puzzle = []
+    for i in range(9):
+        row = string[i*9 : i*10]
+        puzzle.append(row)
+    print(puzzle)
