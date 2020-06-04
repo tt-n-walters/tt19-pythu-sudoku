944a6798b3f995d10bb0896dc428e1671b7e3b89
nw <nico.walters@techtalents.es>
2020-06-04 18:16:16 +0200

    Finished convert function

--- solver.py
+++ solver.py
@@ -10,13 +10,16 @@ def convert_puzzle(string):
     puzzle = []
     for i in range(9):
         row = string[i*9 : i*9+9]
+        row = list(row)
+        row = list(map(int, row))
         puzzle.append(row)
     print(puzzle)
 
 
-import os
-correct_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(correct_dir)
+if __name__ == "__main__":
+    import os
+    correct_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(correct_dir)
 
-puzzles = read_puzzle_file()
-convert_puzzle(puzzles[10])
+    puzzles = read_puzzle_file()
+    convert_puzzle(puzzles[10])
